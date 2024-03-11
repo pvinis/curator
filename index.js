0b5848d6ced2e2acc1b6ee0786e03fc1ffb071e3
Pavlos Vinieratos <pvinis@gmail.com>
2024-03-11 19:25:17 +0200

    fmt

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-const { chromium } = require("playwright")
-const { notifyPavlos } = require("./notify")
+const { chromium } = require("playwright");
+const { notifyPavlos } = require("./notify");
 
-let html = ""
+let html = "";
 
 const fetch = async () => {
-	const browser = await chromium.launch({ headless: true })
-	const context = await browser.newContext()
-	const page = await context.newPage()
+  const browser = await chromium.launch({ headless: true });
+  const context = await browser.newContext();
+  const page = await context.newPage();
 
-	await page.goto("https://tickets.public.gr/theater/oi-gries-pou-mazeuoun-tin-tsouknida-1/")
+  await page.goto(
+    "https://www.more.com/theater/i-anodos-tou-artouro-oui-2os-xronos/"
+  );
 
-	await sleep(3000)
-	html = await page.content()
+  await sleep(3000);
+  html = await page.content();
 
-	// ---------------------
-	await context.close()
-	await browser.close()
-}
+  // ---------------------
+  await context.close();
+  await browser.close();
+};
 
 const shouldNotify = () => {
-	const count = (html.match(/Βίκτωρος Ουγκώ 55/g) || []).length
-	console.log("count is", count)
+  const count = (html.match(/Βίκτωρος Ουγκώ 55/g) || []).length;
+  console.log("count is", count);
 
-	return count > 8
-}
+  return count > 8;
+};
 
 const sleep = (ms) => {
-	return new Promise((resolve) => setTimeout(resolve, ms))
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-;(async () => {
-	console.log("checking..")
-	await fetch()
-	if (shouldNotify()) {
-		console.log("will notify!!")
-		notifyPavlos()
-	} else {
-		console.log("waiting..")
-	}
-})()
+(async () => {
+  console.log("checking..");
+  await fetch();
+  if (shouldNotify()) {
+    console.log("will notify!!");
+    notifyPavlos();
+  } else {
+    console.log("waiting..");
+  }
+})();
